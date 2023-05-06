b17a23146f1a32bbed31c628c5fc797c515f947a
ppusda <ppusda1234@gmail.com>
2023-05-06 17:07:01 +0900

    fix: 이미지 드래그 시 슬라이드 기능 구현 완료

--- front/src/views/board/ReadView.vue
+++ front/src/views/board/ReadView.vue
@@ -4,13 +4,13 @@ import {defineProps, onMounted, ref} from "vue";
 import axios from "axios";
 import router from "@/router";
 
-import Carousel from 'bootstrap/js/dist/carousel'
+import { Carousel } from 'bootstrap';
 
 let isMouseDown = ref(false);
-let lastX = ref<number | null>(null);
-let lastY = ref<number | null>(null);
+let lastX = ref<number | null>(0);
 
-const activeSlideIndex = ref(0);
+const myCarouselElement = ref<HTMLElement|null>(null);
+const carousel = ref<Carousel>();
 
 const props = defineProps({
   postId: {
@@ -40,89 +40,101 @@ function imageLike() {
   });
 }
 
+function handleMouseUp() {
+  isMouseDown.value = false;
+}
+
 function handleMouseDown() {
   isMouseDown.value = true;
 }
 
 function handleMouseMove(event: MouseEvent) {
   if (isMouseDown.value) {
-    const { clientX, clientY } = event;
-    if (lastX.value && lastY.value) {
-      if (clientX > lastX.value) {
+    const {clientX} = event;
+    if (lastX.value) {
+      if (lastX.value - clientX > 1) {
         showNextSlide();
-      } else if (clientX < lastX.value) {
+      } else if (clientX - lastX.value > 1) {
         showPrevSlide();
       }
     }
     lastX.value = clientX;
-    lastY.value = clientY;
+  }else{
+    const {clientX} = event;
+    lastX.value = clientX;
   }
 }
 
-const myCarouselElement = document.querySelector('#imageSlider') as HTMLElement;
-const carousel = new Carousel(myCarouselElement);
-
-function showNextSlide() {
-  carousel.next();
-}
-
-function showPrevSlide() {
-  carousel.prev();
-}
 
 onMounted( () => {
   axios.get(`/nm/boards/${props.postId}`).then((response) => {
     console.log(response.data)
     post.value = response.data;
   });
+
+  myCarouselElement.value = document.querySelector('#imageSlider');
+  if (myCarouselElement.value) {
+    carousel.value = new Carousel(myCarouselElement.value);
+  }
 });
+
+function showNextSlide() {
+  carousel.value?.next();
+}
+
+function showPrevSlide() {
+  carousel.value?.prev();
+}
 
 </script>
 
 <template>
-  <div class="container w-100 h-100 text-white text-center">
-    <div class="content_area" method="post">
-      <h2>{{post.title}}</h2>
-      <div>{{post.content}}</div>
+  <html @mouseup="handleMouseUp">
+    <div class="container w-100 h-100 text-white text-center" @mouseup="handleMouseUp">
+      <div class="content_area" method="post">
+        <h2>{{post.title}}</h2>
+        <div>{{post.content}}</div>
 
-      <div id="imageSlider" class="carousel slide" data-bs-ride="true">
-        <div class="carousel-indicators">
-          <button v-for="index in post.boardImages.length"
-                  :key="index"
-                  type="button"
-                  data-bs-target="#imageSlider"
-                  :data-bs-slide-to="index-1"
-                  :class="{'active': index-1 === 0}"
-                  aria-current="true">
+        <div id="imageSlider" class="carousel slide" data-bs-ride="true">
+          <div class="carousel-indicators">
+            <button v-for="index in post.boardImages.length"
+                    :key="index"
+                    type="button"
+                    data-bs-target="#imageSlider"
+                    :data-bs-slide-to="index-1"
+                    :class="{'active': index-1 === 0}"
+                    aria-current="true">
+            </button>
+          </div>
+          <div class="carousel-inner">
+            <div v-if="post.boardImages && post.boardImages.length > 0"
+                 v-for="(boardImage, index) in post.boardImages"
+                 class="carousel-item"
+                 :class="{'active': index === 0}"
+                 @mousedown="handleMouseDown"
+                 @mousemove="handleMouseMove"
+                 @mouseup="handleMouseUp">
+              <img class="thumbnail" :id="`${boardImage.id}`" :src="`data:image/jpeg;base64,${boardImage.imageBytes}`" @mousedown.prevent />
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#imageSlider" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#imageSlider" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
           </button>
         </div>
-        <div class="carousel-inner">
-          <div v-if="post.boardImages && post.boardImages.length > 0"
-               v-for="(boardImage, index) in post.boardImages"
-               class="carousel-item"
-               :class="{'active': index === 0}"
-               @mousedown="handleMouseDown"
-               @mousemove="handleMouseMove">
-            <img class="thumbnail" :id="`${boardImage.id}`" :src="`data:image/jpeg;base64,${boardImage.imageBytes}`" @mousedown.prevent />
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#imageSlider" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#imageSlider" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
 
-      <div>
-        <a class="clButton btn btn-secondary text-white m-1" @click="$router.go(-1)">취소</a>
-        <a class="clButton btn btn-primary text-white m-1" @click="moveToEdit()">수정</a>
-        <a class="clButton btn btn-danger text-white m-1" @click="imageLike()">❤</a>
+        <div>
+          <a class="clButton btn btn-secondary text-white m-1" @click="$router.go(-1)">취소</a>
+          <a class="clButton btn btn-primary text-white m-1" @click="moveToEdit()">수정</a>
+          <a class="clButton btn btn-danger text-white m-1" @click="imageLike()">❤</a>
+        </div>
       </div>
     </div>
-  </div>
+  </html>
 </template>
 
 <style scoped>
