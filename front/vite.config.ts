cc7ffc47f3b318a105217cde2a09a20ecbd4643c
ppusda <ppusda1234@gmail.com>
2023-05-26 21:33:24 +0900

    불필요한 파일 제거 및 설정 파일 변경

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 
+const path = require('path');
+
+// module.exports = {
+//   outputDir: path.resolve(__dirname, '../src/main/resources/static'),
+// } 후에 한 번에 배포가 가능하다면 시도해 볼 예정
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
