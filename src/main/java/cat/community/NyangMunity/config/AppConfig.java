303cc834f5cc7902b6dc8e71dfce5ded484e724e
ppusda <ppusda1234@gmail.com>
2023-09-23 18:18:23 +0900

    feat: Jwt 인증 적용

--- src/main/java/cat/community/NyangMunity/config/AppConfig.java
+++ src/main/java/cat/community/NyangMunity/config/AppConfig.java
@@ -1,13 +1,25 @@
 package cat.community.NyangMunity.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
-@Configuration
+import java.util.Base64;
+
+@Setter
+@ConfigurationProperties(prefix = "nyangmunity")
 public class AppConfig {
 
-    @Bean
+    public byte[] jwtKey; // .yml 파일 작성에 따라 다양하게 사용가능 String, List, Map, Object...
+
+    public void setJwtKey(String jwtKey) {
+        this.jwtKey = Base64.getDecoder().decode(jwtKey);
+    }
+
+    public byte[] getJwtKey() {
+        return jwtKey;
+    }
+
     public CommonsMultipartResolver multipartResolver() {
         CommonsMultipartResolver resolver = new CommonsMultipartResolver();
         resolver.setDefaultEncoding("utf-8");
--- src/main/java/cat/community/NyangMunity/config/AuthResolver.java
+++ src/main/java/cat/community/NyangMunity/config/AuthResolver.java
@@ -1,9 +1,10 @@
 package cat.community.NyangMunity.config;
 
+import cat.community.NyangMunity.domain.Token;
 import cat.community.NyangMunity.request.UserSession;
-import cat.community.NyangMunity.domain.Session;
 import cat.community.NyangMunity.exception.Unauthorized;
-import cat.community.NyangMunity.repository.SessionRepository;
+import cat.community.NyangMunity.repository.TokenRepository;
+import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
@@ -14,12 +15,14 @@ import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
-    private final SessionRepository sessionRepository;
+    private final TokenRepository tokenRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -30,20 +33,41 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
         if (servletRequest == null) {
-            log.error("servletRequest");
-            throw  new Unauthorized();
+            log.error(">>> ServletRequest Error");
+            throw new Unauthorized();
         }
 
         Cookie[] cookies = servletRequest.getCookies();
         if(cookies.length == 0){
-            log.error("쿠키가 없음");
+            log.error(">>> No Have AccessToken");
             throw new Unauthorized();
         }
 
         String accessToken = cookies[0].getValue();
-        Session session = sessionRepository.findByAccessToken(accessToken)
-                .orElseThrow(Unauthorized::new);
 
-        return new UserSession(session.getUser().getId());
+        try {
+            if(jwtTokenProvider.validateToken(accessToken)){
+                String userId = jwtTokenProvider.getClaims(accessToken).getBody().getSubject();
+                return new UserSession(Long.parseLong(userId), accessToken);
+            } else{
+                String userId = jwtTokenProvider.getClaims(accessToken).getBody().getSubject();
+                Optional<Token> token = tokenRepository.findByUser_Id(Long.parseLong(userId));
+
+                String refreshToken = token.get().getRefreshToken();
+                if(jwtTokenProvider.validateToken(refreshToken)) {
+                    String newAccessToken = jwtTokenProvider.createAccessToken(Long.parseLong(userId));
+                    return new UserSession(Long.parseLong(userId), newAccessToken);
+                }else {
+                    throw new Unauthorized();
+                }
+            }
+
+        } catch (JwtException e) {
+            throw new Unauthorized();
+        }
     }
 }
+
+/*** 쿠키를 이용한 인증 방법
+ *         return new UserSession(session.getUser().getId());
+ */
