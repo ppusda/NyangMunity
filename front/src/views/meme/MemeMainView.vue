f1cbc57f1ebf5fecdad60b853b5d38bb4ca711f1
ppusda <ppusda1234@gmail.com>
2024-01-30 23:23:24 +0900

    refactor: Meme 페이지 디자인 변경 (로딩 제거 및 masonry 스타일 추가)

--- front/src/views/meme/MemeMainView.vue
+++ front/src/views/meme/MemeMainView.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 
-import {onMounted, ref} from "vue";
+import {onMounted, onUnmounted, ref} from "vue";
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 interface Gif {
   title: string;
@@ -12,11 +14,15 @@ interface Gif {
 }
 
 const gifs = ref<Gif[]>([]);
+const memeList = ref();
+
 const { VITE_APP_TENOR_SECRET_KEY } = import.meta.env;
 const { VITE_APP_TENOR_CLIENT_KEY } = import.meta.env;
-const limit = 15;
-let posKey: string[] = []
+const limit = 36;
+
+let posKey: string[] = [];
 let currentPage = -1;
+
 
 function getData(searchTerm: string, page: number): Promise<Gif[]> {
   const search_url = `https://tenor.googleapis.com/v2/search?q=${searchTerm}&key=${VITE_APP_TENOR_SECRET_KEY}&client_key=${VITE_APP_TENOR_CLIENT_KEY}&limit=${limit}&pos=${posKey[page]}`;
@@ -45,115 +51,81 @@ function getData(searchTerm: string, page: number): Promise<Gif[]> {
 async function searchGifs(searchTerm: string, page:number): Promise<void> {
   try {
     const searchResults = await getData(searchTerm, page);
-    gifs.value = searchResults;
+    gifs.value = gifs.value.concat(searchResults);
   } catch (error) {
     console.error(error);
   }
 }
 
-onMounted(() => {
-  searchGifs("cat meme", currentPage);
-});
-
 async function nextPage(): Promise<void> {
   currentPage++;
-  imageLoading();
   await searchGifs("cat meme", currentPage);
 }
 
-async function prevPage(): Promise<void> {
-  if (currentPage >= 0) {
-    currentPage--;
-    imageLoading();
-    await searchGifs("cat meme", currentPage);
-  }else{
-    await searchGifs("cat meme", -1);
-  }
-}
-
-async function imageLoading(): Promise<void> {
-  for(let i=0; i<15; i++){
-    gifs.value[i].media_formats.gif.url = "/assets/images/cat_loading.gif"
-  }
-}//https://gifer.com/en/Uond gif 사용
-
 function copyImageUrl(url: string): void {
   navigator.clipboard.writeText(url).then(() => {
-    showToast("Image Copied!")
+      toast("Image Copied!", {
+        autoClose: 3000,
+      })
   }, (error) => {
     console.error("Failed to copy image URL: ", error);
   });
 }
 
-function showToast(message: string): void {
-  const toast = document.createElement('div');
-  toast.textContent = message;
-  toast.style.position = 'fixed';
-  toast.style.bottom = '50%';
-  toast.style.left = '50%';
-  toast.style.transform = 'translateX(-50%)';
-  toast.style.padding = '10px';
-  toast.style.backgroundColor = '#333';
-  toast.style.color = '#fff';
-  toast.style.borderRadius = '5px';
-  toast.style.opacity = '0.5';
-  toast.style.transition = 'opacity 0.5s ease';
+onMounted(() => {
+  searchGifs("cat meme", currentPage);
+  memeList.value.addEventListener('scroll', handleScroll);
+});
 
-  document.body.appendChild(toast);
+onUnmounted(() => {
+  memeList.value.removeEventListener('scroll', handleScroll);
+});
 
-  setTimeout(() => {
-    toast.style.opacity = '1';
-  }, 100);
-
-  setTimeout(() => {
-    toast.style.opacity = '0';
-    setTimeout(() => {
-      document.body.removeChild(toast);
-    }, 500);
-  }, 1500);
+function handleScroll() {
+  const nearBottom = memeList.value.offsetHeight + memeList.value.scrollTop >= memeList.value.scrollHeight - 500;
+  if (nearBottom) {
+    nextPage();
+  }
 }
 
 </script>
 
 <template>
 
-  <div class="container w-100 h-100 text-white text-center">
-    <div class="content_area w-100">
-      <img v-for="(gif, index) in gifs" class="cmgs m-1 h-100 w-100" :id="`cmg_${index - 1}`" :src="gif.media_formats.gif.url" alt="" @click=copyImageUrl(gif.media_formats.gif.url)>
-      <div class="mt-2">
-        <a class="clButton btn btn-secondary text-white m-1" @click="prevPage">이전페이지</a>
-        <a class="clButton btn btn-primary text-white m-1" @click="nextPage">다음페이지</a>
+  <div class="bg-zinc-800 w-screen rounded-2xl p-4 flex flex-col items-center">
+    <h1 class="text-3xl font-bold text-primary mb-1.5">Nyang Meme</h1>
+    <h1 class="text-white mb-3">이미지 클릭을 통해 복사하실 수 있습니다!</h1>
+    <div class="bg-zinc-700 rounded-md sm:w-6/12 w-full h-[40rem] p-8 overflow-auto memeList" ref="memeList">
+      <div class="masonry">
+        <img v-for="(gif, index) in gifs" :id="`cmg_${index}`" :src="gif.media_formats.gif.url" alt="" @click=copyImageUrl(gif.media_formats.gif.url)>
       </div>
     </div>
   </div>
 
-  <div>
-    <b></b>
-  </div>
-
 </template>
 
-<style scoped>
-  .content_area {
-    top: 1.5%;
-    padding: 1vw;
-  }
+<style>
+.masonry {
+  max-width: 100%;  /* 컨테이너의 최대 너비를 설정 */
+  margin: auto;  /* 컨테이너를 화면 중앙에 위치시킴 */
+  column-count: 5;
+  column-gap: 0.5em;  /* 이미지 간의 간격을 줄임 */
+}
 
-  .cmgs {
-    max-width: 220px;
-    max-height: 164px;
-    object-fit: cover;
-  }
+.masonry img {
+  width: 100%;
+  max-height: 100%;
+  object-fit: cover;
+  margin-top: 0.5em;
+}
 
-  .content_area{
-    overflow-y: scroll;
-    max-height: 80vh;
-    -ms-overflow-style: none;
-    scrollbar-width: none;
-  }
+.memeList{
+  overflow-y: scroll;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+}
 
-  .content_area::-webkit-scrollbar {
-    display:none;
-  }
-
+.memeList::-webkit-scrollbar {
+  display:none;
+}
 </style>
