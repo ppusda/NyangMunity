ea500a9490d5a51655c5060089d3448e8009719c
ppusda <ppusda1234@gmail.com>
2024-05-09 17:36:14 +0900

    refactor: 로그인 페이지 수정

--- front/src/views/user/LoginView.vue
+++ front/src/views/user/LoginView.vue
@@ -28,41 +28,39 @@ const login = function () {
 </script>
 
 <template>
-  <input value="true" type="text" name="loginState" id="loginState" hidden="true"/>
-
-  <div class="h-100">
-    <div class="bg-zinc-800 w-screen rounded-2xl p-16">
-      <div class="flex flex-col justify-center items-center">
-        <h1 class="text-3xl font-bold text-white mb-12">로그인</h1>
-        <form class="bg-zinc-700 p-5 rounded-md sm:w-6/12 w-full">
-          <div class="flex flex-col m-3">
-            <label for="email" class="text-white mb-1">아이디</label>
-            <input type="email" name="email" id="email" v-model="email" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
-          </div>
-          <div class="flex flex-col m-3">
-            <label for="password" class="text-white mb-1">비밀번호</label>
-            <input type="password" name="password" id="password" v-model="password" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="비밀번호를 입력해주세요.">
-          </div>
-          <div class="flex flex-col justify-center mt-10 m-3">
-            <a @click="login" class="btn btn-outline btn-primary w-full rounded-md text-white mb-1.5"><i class="fa-solid fa-door-open"></i> 로그인</a>
-            <a class="btn btn-outline btn-warning w-full p-3 rounded-md text-black" :href="`https://kauth.kakao.com/oauth/authorize?client_id=${VITE_APP_KAKAO_CLIENT_KEY}&redirect_uri=${VITE_APP_KAKAO_REDIRECT_URL}&response_type=code`">
-              <i class="fa-solid fa-comment fa-flip-horizontal"></i>
-              카카오 로그인
-            </a>
-          </div>
-          <div class="mt-3 mb-3 text-white m-5 text-center">
-            <p>------------- 처음이신가요? -------------</p>
-          </div>
-          <div class="flex flex-col m-3">
-            <a class="btn btn-outline btn-ghost w-full p-3 rounded-md text-white">
-              <i class="fa-solid fa-user-plus"></i>
-              <router-link to="/user/join">회원가입</router-link>
-            </a>
-          </div>
-        </form>
-      </div>
+  <div class="w-screen rounded-2xl p-12 justify-center">
+    <div class="flex flex-col justify-center items-center">
+      <img id="logo" class="h-[80px]" src="/src/images/logo.png" alt="logo">
+      <h1 class="text-3xl font-bold text-white mb-12">냥뮤니티에 로그인하세요!</h1>
+      <form class="bg-zinc-800 p-5 rounded-md">
+        <div class="flex flex-col m-3">
+          <label for="email" class="text-white mb-1">아이디</label>
+          <input type="email" name="email" id="email" v-model="email" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
+        </div>
+        <div class="flex flex-col m-3">
+          <label for="password" class="text-white mb-1">비밀번호</label>
+          <input type="password" name="password" id="password" v-model="password" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="비밀번호를 입력해주세요.">
+        </div>
+        <div class="flex flex-col justify-center mt-10 m-3">
+          <a @click="login" class="btn btn-outline btn-primary w-full rounded-md text-white mb-1.5"><i class="fa-solid fa-door-open"></i> 로그인</a>
+          <a class="btn btn-outline btn-warning w-full p-3 rounded-md text-black" :href="`https://kauth.kakao.com/oauth/authorize?client_id=${VITE_APP_KAKAO_CLIENT_KEY}&redirect_uri=${VITE_APP_KAKAO_REDIRECT_URL}&response_type=code`">
+            <i class="fa-solid fa-comment fa-flip-horizontal"></i>
+            카카오 로그인
+          </a>
+        </div>
+        <div class="mt-3 mb-3 text-white m-5 text-center">
+          <p>------------- 처음이신가요? -------------</p>
+        </div>
+        <div class="flex flex-col m-3">
+          <a class="btn btn-outline btn-ghost w-full p-3 rounded-md text-white">
+            <i class="fa-solid fa-user-plus"></i>
+            <router-link to="/user/join">회원가입</router-link>
+          </a>
+        </div>
+      </form>
     </div>
   </div>
+
 
 </template>
 
