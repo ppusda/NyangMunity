d86fc664d4820dfe3c1d54dec10cc8c438362659
ppusda <ppusda1234@gmail.com>
2024-05-10 23:23:23 +0900

    refactor: 파일명 및 구조 일부 수정

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,24 +1,14 @@
-<script lang="ts">
-  import Header from "./views/commons/Header.vue";
-  import MainView from "./views/MainView.vue";
-
-import "./assets/main.css";
-
-export default {
-  name: 'App',
-  components: {
-    Header,
-    MainView
-  }
-}
-</script>
-
 <template>
   <div id="app" class="flex flex-col h-screen bg-zinc-900">
     <Header />
     <router-view />
   </div>
 </template>
+
+<script setup lang="ts">
+  import Header from "@/components/Header.vue";
+  import "./assets/main.css";
+</script>
 
 <style>
 
--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,13 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainView from "../views/MainView.vue";
-import WriteView from "../views/board/WriteView.vue";
-import BoardListView from "../views/board/BoardListView.vue";
-import EditView from "../views/board/EditView.vue";
-import LoginView from "../views/user/LoginView.vue";
-import JoinView from "../views/user/JoinView.vue";
-import UserView from "../views/user/UserView.vue";
-import MemeView from "../views/meme/MemeMainView.vue";
-
+import WriteView from "../views/board/Write.vue";
+import BoardListView from "../views/board/List.vue";
+import EditView from "../views/board/Edit.vue";
+import LoginView from "../views/user/Login.vue";
+import JoinView from "../views/user/Join.vue";
+import UserView from "../views/user/User.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
