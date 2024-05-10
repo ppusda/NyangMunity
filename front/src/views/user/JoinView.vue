40ad73185cd0f27615978249e2ce6d7a9def9c21
ppusda <ppusda1234@gmail.com>
2024-05-10 21:24:17 +0900

    refactor: 로그인 페이지 회원가입 페이지 수정

--- front/src/views/user/JoinView.vue
+++ front/src/views/user/JoinView.vue
@@ -28,41 +28,36 @@ const joinConfirm = function () {
 </script>
 
 <template>
-  <div class="h-100">
-    <div class="bg-zinc-800 w-screen rounded-2xl p-10">
-      <div class="flex flex-col justify-center items-center">
-        <h1 class="text-3xl font-bold text-white mb-12">회원가입</h1>
-        <form class="bg-zinc-700 p-5 rounded-md sm:w-6/12 w-full">
-          <div class="flex flex-col m-3">
-            <label for="email" class="text-white mb-1">아이디</label>
-            <input type="email" name="email" id="email" v-model="email" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
+  <div class="w-screen rounded-2xl p-12">
+    <div class="flex flex-col justify-center items-center">
+      <img id="logo" class="h-[80px]" src="/src/images/logo.png" alt="logo">
+      <h1 class="text-3xl font-bold text-white mb-12">냥뮤니티 회원가입</h1>
+      <form class="bg-zinc-800 p-5 rounded-md sm:w-[25rem] w-[26rem]">
+        <div class="flex flex-col m-3">
+          <label for="email" class="text-white mb-1">아이디</label>
+          <input type="email" name="email" id="email" v-model="email" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
+        </div>
+        <div class="flex flex-col m-3">
+          <label for="password" class="text-white mb-1">비밀번호</label>
+          <input type="password" name="password" id="password" v-model="password" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="비밀번호를 입력해주세요.">
+        </div>
+        <div class="flex flex-col m-3">
+          <label for="passwordChk" class="text-white mb-1">비밀번호 확인</label>
+          <input type="password" name="passwordChk" id="passwordChk" v-model="passwordChk" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="비밀번호를 재입력해주세요.">
+        </div>
+        <div class="flex flex-col m-3">
+          <label for="nickname" class="text-white mb-1">닉네임</label>
+          <input type="text" name="nickname" id="nickname" v-model="nickname" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="닉네임을 입력해주세요.">
+        </div>
+        <div class="flex flex-row justify-center mt-10 m-3 w-full gap-2 pr-5">
+          <div class="flex-1">
+            <a class="btn btn-outline btn-error p-3 rounded-md text-black w-full" @click="$router.go(-1)">취소</a>
           </div>
-          <div class="flex flex-col m-3">
-            <label for="password" class="text-white mb-1">비밀번호</label>
-            <input type="password" name="password" id="password" v-model="password" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="비밀번호를 입력해주세요.">
+          <div class="flex-1">
+            <a class="btn btn-outline btn-primary p-3 rounded-md text-white w-full" @click="joinConfirm">완료</a>
           </div>
-          <div class="flex flex-col m-3">
-            <label for="passwordChk" class="text-white mb-1">비밀번호 확인</label>
-            <input type="password" name="passwordChk" id="passwordChk" v-model="passwordChk" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="비밀번호를 재입력해주세요.">
-          </div>
-          <div class="flex flex-col m-3">
-            <label for="date" class="text-white mb-1">생일</label>
-            <input type="date" name="birthday" id="birthday" v-model="birthday" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
-          </div>
-          <div class="flex flex-col m-3">
-            <label for="nickname" class="text-white mb-1">닉네임</label>
-            <input type="text" name="nickname" id="nickname" v-model="nickname" class="input input-bordered border-zinc-500 bg-zinc-700 w-full p-3 rounded-md text-white" placeholder="닉네임을 입력해주세요.">
-          </div>
-          <div class="flex flex-row justify-center mt-10 m-3 w-full gap-2 pr-5">
-            <div class="flex-1">
-              <a class="btn btn-outline btn-error p-3 rounded-md text-black w-full" @click="$router.go(-1)">취소</a>
-            </div>
-            <div class="flex-1">
-              <a class="btn btn-outline btn-primary p-3 rounded-md text-white w-full" @click="joinConfirm">완료</a>
-            </div>
-          </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
--- front/src/views/user/LoginView.vue
+++ front/src/views/user/LoginView.vue
@@ -32,7 +32,7 @@ const login = function () {
     <div class="flex flex-col justify-center items-center">
       <img id="logo" class="h-[80px]" src="/src/images/logo.png" alt="logo">
       <h1 class="text-3xl font-bold text-white mb-12">냥뮤니티에 로그인하세요!</h1>
-      <form class="bg-zinc-800 p-5 rounded-md">
+      <form class="bg-zinc-800 p-5 rounded-md sm:w-[25rem] w-[26rem]">
         <div class="flex flex-col m-3">
           <label for="email" class="text-white mb-1">아이디</label>
           <input type="email" name="email" id="email" v-model="email" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="이메일을 입력해주세요.">
@@ -41,26 +41,25 @@ const login = function () {
           <label for="password" class="text-white mb-1">비밀번호</label>
           <input type="password" name="password" id="password" v-model="password" class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-3 rounded-md text-white" placeholder="비밀번호를 입력해주세요.">
         </div>
-        <div class="flex flex-col justify-center mt-10 m-3">
+        <div class="flex flex-col justify-center mt-8 m-3">
           <a @click="login" class="btn btn-outline btn-primary w-full rounded-md text-white mb-1.5"><i class="fa-solid fa-door-open"></i> 로그인</a>
-          <a class="btn btn-outline btn-warning w-full p-3 rounded-md text-black" :href="`https://kauth.kakao.com/oauth/authorize?client_id=${VITE_APP_KAKAO_CLIENT_KEY}&redirect_uri=${VITE_APP_KAKAO_REDIRECT_URL}&response_type=code`">
-            <i class="fa-solid fa-comment fa-flip-horizontal"></i>
-            카카오 로그인
-          </a>
-        </div>
-        <div class="mt-3 mb-3 text-white m-5 text-center">
-          <p>------------- 처음이신가요? -------------</p>
-        </div>
-        <div class="flex flex-col m-3">
-          <a class="btn btn-outline btn-ghost w-full p-3 rounded-md text-white">
-            <i class="fa-solid fa-user-plus"></i>
-            <router-link to="/user/join">회원가입</router-link>
-          </a>
+          <div class="flex flex-row justify-center m-3 mb-0">
+            <a class="btn btn-outline btn-warning p-3 rounded-md w-4/12 mr-3"><i class="fa-solid fa-comment fa-flip-horizontal"></i></a>
+            <a class="btn btn-outline btn-ghost p-3 rounded-md text-white w-4/12 mr-3"><i class="fa-brands fa-google"></i></a>
+            <a class="btn btn-outline btn-ghost p-3 rounded-md text-black w-4/12"><i class="fa-brands fa-github"></i></a>
+          </div>
         </div>
       </form>
     </div>
+    <div class="flex flex-col justify-center items-center mt-3">
+      <div class="bg-zinc-800 px-5 py-3 rounded-md sm:w-[25rem] w-[26rem]">
+        <div class="flex flex-col m-1.5 text-center">
+          <h6 class="text-md font-bold text-white mb-3">처음 이용하시나요?</h6>
+          <router-link to="/user/join" class="btn btn-outline btn-ghost w-full p-3 rounded-md text-white"><i class="fa-solid fa-user-plus"></i>회원가입</router-link>
+        </div>
+      </div>
+    </div>
   </div>
-
 
 </template>
 
