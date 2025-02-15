aca68c6e7ff55cf9f2285ebc323828fd2a073a1a
ppusda <ppusda1234@gmail.com>
2025-02-15 19:33:29 +0900

    test: 테스트 코드 제거 (구조 변경 후 재작성 예정)

--- src/test/java/cat/community/NyangMunity/NyangMunityApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package cat.community.NyangMunity;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class NyangMunityApplicationTests {
-
-}
