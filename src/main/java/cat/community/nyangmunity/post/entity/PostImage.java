32bbc6dcae216916ad400332e75760c48af3c172
ppusda <ppusda1234@gmail.com>
2025-03-04 00:12:27 +0900

    feat: PostImage Entity 추가

---
+++ src/main/java/cat/community/nyangmunity/post/entity/PostImage.java
@@ -0,0 +1,29 @@
+package cat.community.nyangmunity.post.entity;
+
+import cat.community.nyangmunity.image.entity.Image;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Getter
+@NoArgsConstructor
+public class PostImage {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "image_id")
+	private Image image;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
+}
