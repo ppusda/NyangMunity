96fa1ad1b513b179d44c6e90b478a2c718de74ab
ppusda <ppusda1234@gmail.com>
2025-04-04 00:00:17 +0900

    refactor: 회원정보 Page 수정

--- front/src/views/member/Member.vue
+++ front/src/views/member/Member.vue
@@ -29,7 +29,7 @@ const userEdit = function () {
         member.password = newPassword.value;
 
         formData.append("nickname", member.nickname);
-        formData.append("password",  member.password);
+        formData.append("password", member.password);
         formData.append("birthday", member.birthday);
 
         axios.post("/nm/members/edit", formData).then(() => {
@@ -74,92 +74,125 @@ onMounted(async () => {
 });
 
 </script>
-
 <template>
-  <div class="container login_page w-100 h-100 text-white text-center bootstrap snippets">
-    <div class="content_area">
-      <h3>User info</h3>
-      <hr />
-      <div class="member-info">
-        <table>
-          <tr>
-            <td rowspan="6">
-              <img class="thumbnail avatar img-circle img-thumbnail" src="/assets/images/cat_loading.gif" @mousedown.prevent/>
-              <div type="button" class="btn-upload">파일 업로드하기</div>
-            </td>
-          </tr>
-          <tr>
-            <td class="w-25"><a class="text-white">이메일 : </a></td>
-            <td class="w-50">
-              <a class="w-100" id="email">{{member.email}}</a>
-            </td>
-          </tr>
-          <tr>
-            <td class="w-25"><a class="text-white">닉네임 : </a></td>
-            <td class="w-50">
-              <input class="w-100" id="nickname" name="nickname" type="text" v-model="member.nickname"/>
-            </td>
-          </tr>
-          <tr>
-            <td class="w-25"><a class="text-white">새 비밀번호 : </a></td>
-            <td class="w-50">
-              <input class="w-100" v-model="newPassword" id="newPassword" type="password" />
-            </td>
-          </tr>
-          <tr>
-            <td class="w-25"><a class="text-white">새 비밀번호 확인 : </a></td>
-            <td class="w-50">
-              <input class="w-100" v-model="newPasswordChk" id="newPasswordChk" type="password" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <input id="passwordCheck" type="password" placeholder="현재 비밀번호 입력" v-model="pwdCheck"/>
-            </td>
-            <td colspan="2">
-              <a class="clButton btn btn-primary text-white m-1" @click="userEdit()">수정</a>
-              <a class="clButton btn btn-danger text-white m-1" data-bs-toggle="modal" data-bs-target="#cancelUserModal">회원탈퇴</a>
+  <div class="w-screen rounded-2xl p-8 flex justify-center">
+    <div class="bg-zinc-800 p-6 rounded-md sm:w-[30rem] w-[32rem]">
+      <div class="text-center mb-6">
+        <h3 class="text-2xl font-bold text-white">User info</h3>
+        <hr class="border-gray-600 mt-3" />
+      </div>
 
-              <div class="modal fade" id="cancelUserModal" tabindex="-1" role="dialog" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title text-black" >탈퇴</h5>
-                    </div>
-                    <div class="modal-body text-black">
-                      <b>정말로 탈퇴하시겠습니까? (* 다시 되돌릴 수 없습니다.)</b>
-                    </div>
-                    <div class="modal-footer">
-                      <a type="button" class="clButton btn btn-secondary text-white m-1" data-bs-dismiss="modal">취소</a>
-                      <a type="button" class="clButton btn btn-danger text-white m-1" @click="cancelUser()" data-bs-dismiss="modal">확인</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </td>
-          </tr>
-        </table>
+      <div class="flex flex-col items-center mb-6">
+        <img class="w-24 h-24 rounded-full object-cover mb-2"
+             src="/assets/images/cat_loading.gif"
+             @mousedown.prevent />
+        <button class="btn btn-outline btn-ghost text-sm text-white px-4 py-1 rounded-md">
+          파일 업로드하기
+        </button>
+      </div>
+
+      <div class="flex flex-col space-y-4">
+        <div class="flex flex-row items-center">
+          <label class="text-white w-32">이메일 :</label>
+          <p id="email" class="text-white flex-1">{{member.email}}</p>
+        </div>
+
+        <div class="flex flex-row items-center">
+          <label for="nickname" class="text-white w-32">닉네임 :</label>
+          <input
+              class="flex-1 input input-bordered border-zinc-500 bg-zinc-900 p-2 rounded-md text-white"
+              id="nickname"
+              name="nickname"
+              type="text"
+              v-model="member.nickname"/>
+        </div>
+
+        <div class="flex flex-row items-center">
+          <label for="newPassword" class="text-white w-32">새 비밀번호 :</label>
+          <input
+              class="flex-1 input input-bordered border-zinc-500 bg-zinc-900 p-2 rounded-md text-white"
+              v-model="newPassword"
+              id="newPassword"
+              type="password" />
+        </div>
+
+        <div class="flex flex-row items-center">
+          <label for="newPasswordChk" class="text-white w-32">비밀번호 확인 :</label>
+          <input
+              class="flex-1 input input-bordered border-zinc-500 bg-zinc-900 p-2 rounded-md text-white"
+              v-model="newPasswordChk"
+              id="newPasswordChk"
+              type="password" />
+        </div>
+
+        <div class="pt-4 border-t border-gray-600">
+          <input
+              id="passwordCheck"
+              type="password"
+              placeholder="현재 비밀번호 입력"
+              v-model="pwdCheck"
+              class="input input-bordered border-zinc-500 bg-zinc-900 w-full p-2 rounded-md text-white mb-4" />
+
+          <div class="flex justify-center gap-3">
+            <button
+                class="btn btn-outline btn-primary text-white px-6 py-2 rounded-md"
+                @click="userEdit()">
+              <i class="fa-solid fa-check"></i> 수정
+            </button>
+            <button
+                class="btn btn-outline btn-error text-white px-6 py-2 rounded-md"
+                data-bs-toggle="modal"
+                data-bs-target="#cancelUserModal">
+              <i class="fa-solid fa-user-slash"></i> 회원탈퇴
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- 회원탈퇴 모달 -->
+      <div class="modal fade" id="cancelUserModal" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content bg-zinc-700">
+            <div class="modal-header border-gray-600">
+              <h5 class="modal-title text-white font-bold">탈퇴</h5>
+            </div>
+            <div class="modal-body text-white">
+              <b>정말로 탈퇴하시겠습니까? (* 다시 되돌릴 수 없습니다.)</b>
+            </div>
+            <div class="modal-footer border-gray-600">
+              <button
+                  class="btn btn-outline btn-ghost text-white px-4 py-2 rounded-md"
+                  data-bs-dismiss="modal">
+                취소
+              </button>
+              <button
+                  class="btn btn-outline btn-error text-white px-4 py-2 rounded-md"
+                  @click="cancelUser()"
+                  data-bs-dismiss="modal">
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.btn-upload {
-  width: 150px;
-  height: 30px;
-  background: #333;
-  border: 1px solid rgb(77, 77, 77);
-  border-radius: 10px;
-  font-weight: 500;
-  cursor: pointer;
-  display: inline-block;
-  margin-top: 0.5vw;
-  align-items: center;
-  justify-content: center;
+.btn-outline {
+  border-width: 1px;
 }
 
-.member-info {
-  display: flex;
+.modal-content {
+  border-radius: 0.5rem;
+}
+
+.modal-header, .modal-footer {
+  padding: 0.75rem 1rem;
+}
+
+.modal-body {
+  padding: 1.5rem;
 }
 </style>
