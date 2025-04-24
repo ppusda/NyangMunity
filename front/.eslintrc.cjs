caab18c7b076ecd9f4f76174cd0d8caa720424e5
ppusda <ppusda1234@gmail.com>
2025-04-24 21:55:56 +0900

    fix: eslint prettier 오류 수정

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -2,14 +2,16 @@
 require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier",
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+    },
+    "editor.codeActionsOnSave": {"source.fixAll": true},
+    "editor.formatOnSave": false,
 };
