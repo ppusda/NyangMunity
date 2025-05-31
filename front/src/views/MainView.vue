f9f0482fa0eb77e789ac95d8713e978014618469
ppusda <ppusda1234@gmail.com>
2025-05-31 22:14:46 +0900

    fix: 메인화면 모바일 환경에 맞춰 수정

--- front/src/views/MainView.vue
+++ front/src/views/MainView.vue
@@ -28,40 +28,40 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex flex-row h-min w-full content-center">
-    <div class="flex flex-row text-white h-min w-full mt-5">
-      <div class="hero min-w-min accent-neutral-900">
-        <div class="hero-content flex-col lg:flex-row-reverse ml-5 p-36">
-          <div class="flex-row">
-            <h1 class="text-7xl font-bold">Nyangmunity</h1>
-            <p class="py-6">당신이 가진 가장 귀여운 고양이 사진을 공유해보세요!</p>
-            <router-link :to="{name: 'posts'}" class="btn btn-outline btn-ghost">
+  <div class="flex flex-col lg:flex-row h-min w-full content-center">
+    <div class="flex flex-col lg:flex-row text-white h-min w-full mt-5">
+      <div class="hero min-w-min accent-neutral-900 w-full lg:w-1/2">
+        <div class="hero-content flex-col lg:flex-row-reverse p-8 lg:p-36">
+          <div class="flex-row text-center lg:text-left">
+            <h1 class="text-4xl lg:text-7xl font-bold">Nyangmunity</h1>
+            <p class="py-6 text-sm lg:text-base">당신이 가진 가장 귀여운 고양이 사진을 공유해보세요!</p>
+            <router-link :to="{name: 'posts'}" class="btn btn-outline btn-ghost text-sm lg:text-base">
               <i class="fa-solid fa-arrow-right"></i>커뮤니티로 바로가기!
             </router-link>
           </div>
         </div>
       </div>
-      <div class="hero min-w-min accent-neutral-900">
-        <div class="hero-content flex-col lg:flex-row-reverse ml-5 p-36">
-          <div>
-            <div v-if="likePostFlg">
+      <div class="hero min-w-min accent-neutral-900 w-full lg:w-1/2">
+        <div class="hero-content flex-col lg:flex-row-reverse p-8 lg:p-36">
+          <div class="w-full">
+            <div v-if="likePostFlg" class="text-center">
               <img
                   v-if="likePost.postImages && likePost.postImages.length > 0"
-                  class="rounded"
+                  class="rounded mx-auto w-full max-w-xs lg:max-w-none"
                   id="main_img"
                   :src="likePost.postImages[0].url"
               />
               <router-link
-                  class="btn btn-outline btn-ghost"
+                  class="btn btn-outline btn-ghost mt-4 text-sm lg:text-base block"
                   :to="{name: 'read', params: {postId: likePost.id}}"
               >
                 {{ likePost.message }}
               </router-link>
             </div>
-            <div v-else class="w-4/6 text-center">
-              <img class="rounded-2xl" src="/assets/images/cat_loading.gif"/>
-              <p class="py-3">{{ likePost.message }}</p>
-              <p class="btn btn-outline btn-ghost">
+            <div v-else class="w-full text-center">
+              <img class="rounded-2xl mx-auto w-full max-w-xs lg:max-w-none" src="/assets/images/cat_loading.gif"/>
+              <p class="py-3 text-sm lg:text-base">{{ likePost.message }}</p>
+              <p class="btn btn-outline btn-ghost text-sm lg:text-base">
                 메인 페이지의 주인공에 도전해보세요!
               </p>
             </div>
