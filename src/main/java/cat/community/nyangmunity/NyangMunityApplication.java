069c812be72abc133555b161724e12bf741783c0
ppusda <ppusda1234@gmail.com>
2025-06-10 22:46:11 +0900

    fix: JpaAuditing 활성화

--- src/main/java/cat/community/nyangmunity/NyangMunityApplication.java
+++ src/main/java/cat/community/nyangmunity/NyangMunityApplication.java
@@ -3,6 +3,7 @@ package cat.community.nyangmunity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import cat.community.nyangmunity.global.config.AppConfig;
@@ -10,6 +11,7 @@ import cat.community.nyangmunity.postImage.image.config.TenorConfig;
 import cat.community.nyangmunity.member.provider.KakaoAuthProvider;
 
 @EnableScheduling
+@EnableJpaAuditing
 @SpringBootApplication
 @EnableConfigurationProperties({AppConfig.class, TenorConfig.class, KakaoAuthProvider.class})
 public class NyangMunityApplication {
